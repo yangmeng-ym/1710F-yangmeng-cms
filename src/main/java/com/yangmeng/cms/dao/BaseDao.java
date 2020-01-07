212910161e235be82bf07990f4a13483b6b0c68e
吴世勋男朋友 <@LAPTOP-8GG4839A>
2020-01-07 20:04:06 +0800

    cms

---
+++ src/main/java/com/yangmeng/cms/dao/BaseDao.java
@@ -0,0 +1,57 @@
+package com.yangmeng.cms.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.yangmeng.cms.user.User;
+
+public interface BaseDao<T> {
+	
+	/**
+	 * @Title: select   
+	 * @Description: 按条件查询   
+	 * @param: @param user
+	 * @param: @return      
+	 * @return: List<User>      
+	 * @throws
+	 */
+	List<User> select(User user);
+	/**
+	 * @Title: selectById   
+	 * @Description: 根据Id查询   
+	 * @param: @param id
+	 * @param: @return      
+	 * @return: User      
+	 * @throws
+	 */
+	User selectById(Integer id);
+	/**
+	 * @Title: insert   
+	 * @Description: 新增   
+	 * @param: @param user
+	 * @param: @return      
+	 * @return: int      
+	 * @throws
+	 */
+	int insert(User user);
+	/**
+	 * @Title: update   
+	 * @Description: 修改   
+	 * @param: @param user
+	 * @param: @return      
+	 * @return: int      
+	 * @throws
+	 */
+	int update(User user);
+	/**
+	 * @Title: delete   
+	 * @Description: 删除   
+	 * @param: @param ids
+	 * @param: @return      
+	 * @return: int      
+	 * @throws
+	 */
+	int delete(@Param("ids") String ids);
+	
+}
---
+++ src/main/java/com/yangmeng/cms/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.yangmeng.cms.dao;
+
+import com.yangmeng.cms.user.User;
+
+public interface UserDao extends BaseDao<User>{
+
+}
